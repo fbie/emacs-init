f34a7339456e5cb647955bb6ac1f66ddda98f7b7
Florian Biermann <flbm@simcorp.com>
2021-04-15 06:10:57 +0100

    Merge when-expressions for the same condition

--- fb-simcorp.el
+++ fb-simcorp.el
@@ -3,17 +3,14 @@
 
 (when (eq system-type 'windows-nt)
   (require 'cygwin-mount)
-  (require 'setup-cygwin))
+  (require 'setup-cygwin)
 
-(defconst fb/is-windows (eq system-type 'windows-nt))
+  (eval-after-load 'magit
+    (setq magit-git-executable "c:/Program Files/Git/bin/git.exe"))
 
-(eval-after-load 'magit
-  (when fb/is-windows ;; Never use Cygwin's outdated version.
-      (setq magit-git-executable "c:/Program Files/Git/bin/git.exe")))
-
-(eval-after-load 'merlin
-  (let ((ml-mono-dir "C:/Repos/ml-mono/"))
-    (when (and fb/is-windows (file-directory-p ml-mono-dir))
-      (setq merlin-command (concat ml-mono-dir "tools/merlin/ocamlmerlin.exe")))))
+  (eval-after-load 'merlin
+    (let ((ml-mono-dir "C:/Repos/ml-mono/"))
+      (when (file-directory-p ml-mono-dir)
+        (setq merlin-command (concat ml-mono-dir "tools/merlin/ocamlmerlin.exe"))))))
 
 (provide 'fb-simcorp)
