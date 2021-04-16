d2f519dfb6c1c126cbb53f421cc426da6125a350
Florian Biermann <flbm@simcorp.com>
2021-04-16 13:38:57 +0100

    eval-after-load wants a single block of code

--- fb-simcorp.el
+++ fb-simcorp.el
@@ -14,7 +14,10 @@
         (setq merlin-command (concat ml-mono-dir "tools/merlin/ocamlmerlin.exe")))))
 
   (eval-after-load 'eglot-fsharp
-    (when (eq eglot-fsharp-server-runtime 'net-framework)
-      (setq inferior-fsharp-program "c:/Program Files (x86)/Microsoft SDKs/F#/4.0/Framework/v4.0/FsiAnyCPU.exe"))))
+    (progn
+      (require 'eglot-fsharp)
+      (when (eq eglot-fsharp-server-runtime 'net-framework)
+        (setq inferior-fsharp-program "c:/Program Files (x86)/Microsoft SDKs/F#/4.0/Framework/v4.0/FsiAnyCPU.exe"
+              eglot-fsharp-server-version "0.41.1")))))
 
 (provide 'fb-simcorp)
