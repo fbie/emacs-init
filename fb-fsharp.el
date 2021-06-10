67cf64127064f6602375b5c48541d6f0480246ea
Florian Biermann <flbm@simcorp.com>
2021-06-10 14:34:17 +0200

    Add fsharp-eval-buffer and bind to C-c C-b

--- fb-fsharp.el
+++ fb-fsharp.el
@@ -2,13 +2,21 @@
 
 (use-package fsharp-mode
   :after eglot
-  :bind (:map fsharp-mode-map ("C-c x" . flycheck-next-error))
   :init
   (add-to-list 'auto-mode-alist '("\\.fsproj$" . xml-mode))
   :config
   (setq fsharp-doc-idle-delay 1.0
         inferior-fsharp-program "dotnet fsi")
-  (add-to-list 'helm-boring-file-regexp-list "^obj/"))
+  (add-to-list 'helm-boring-file-regexp-list "^obj/")
+  (defun fsharp-eval-buffer ()
+    "Evaluate current buffer in FSI."
+    (interactive)
+    (save-mark-and-excursion
+      (fsharp-eval-region (point-min) (point-max))))
+  :bind
+  (:map fsharp-mode-map
+        ("C-c x" . flycheck-next-error)
+        ("C-c C-b" . fsharp-eval-buffer)))
 
 (use-package eglot-fsharp
   :after
